b5c18e398e4899d0f634dd0c7cec9b06b6d68267
rickchiu <rick@jgallop.com>
2023-12-11 00:33:57 +0800

    add baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'tanstack-learning', // 基本 URL 路徑，根據不同模式設定不同路徑
   plugins: [react()],
-})
+});
